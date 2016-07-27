18e83ad561b820bafd71ca54e50be504ffb81c97
lansMX <navarretesalgado.lans@gmail.com>
2016-07-27 14:20:16 -0500

    Add project files.

---
+++ TestRepository/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestRepository.Startup))]
+namespace TestRepository
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
